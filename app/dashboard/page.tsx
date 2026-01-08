9d5b428cf12cb61563e3c6455ed1c7edd082bc63
HarryDang <danghuuphuc001@gmail.com>
2026-01-08 18:06:09 +0700

    fix: import error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,9 @@
-import { RichAreaChart } from "@/components/charts/rich-area-chart";
-import { RichPieChart } from "@/components/charts/rich-pie-chart";
+import { Metadata } from "next";
+import { Prettify, SearchParams } from "@/types";
+
 import { SidebarInsetContent } from "@/components/chunks/sidebar-chunks";
 import { DashboardKanban } from "@/components/segments/dashboard/dashboard-kanban";
 
-import { StatisticCard } from "@/components/segments/locations/card-block/statistic-card";
 import { DataGridDemo } from "@/components/data-grid/data-grid-demo";
 import DataGridRenderPage from "@/components/data-grid/data-grid-render/data-grid-render-page";
 import {
@@ -11,11 +11,6 @@ import {
   DataGridSkeletonGrid,
   DataGridSkeletonToolbar,
 } from "@/components/data-grid/data-grid-skeleton";
-import TaskTableWrapper from "@/components/data-table/task-table/task-table-wrapper";
-import { Prettify, SearchParams } from "@/types";
-import { UserIcon } from "lucide-react";
-import { Metadata } from "next";
-import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Statistics",
