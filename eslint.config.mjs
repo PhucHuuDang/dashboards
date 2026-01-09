04cb302981e48b9f9f2154901fae68a013321635
HarryDang <danghuuphuc001@gmail.com>
2026-01-09 10:14:04 +0700

    feat(eslint-rules): rules ordering import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,76 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import importPlugin from "eslint-plugin-import";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin", // fs, path
+            "external", // react, next, lucide-react
+            "internal", // @/...
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "next/**",
+              group: "external",
+              position: "before",
+            },
+
+            {
+              pattern: "react-use/**",
+              group: "external",
+              position: "before",
+            },
+
+            {
+              pattern: "@/types/**",
+              group: "internal",
+              position: "before",
+            },
+
+            {
+              pattern: "@/mocks/**",
+              group: "internal",
+              position: "before",
+            },
+
+            {
+              pattern: "@/**",
+              group: "internal",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["react"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
