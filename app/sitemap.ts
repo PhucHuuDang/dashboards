7ba76edcc45e64367e9413e8fb0aacfa9d888eaf
HarryDang <danghuuphuc001@gmail.com>
2026-01-26 15:28:37 +0700

    chore(sitemap): change sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,15 @@
+import { siteConfig } from "@/config/site";
+import { ROUTES } from "@/constant/routes";
+
 import type { MetadataRoute } from "next";
 
-import { siteConfig } from "@/config/site";
-
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = [""].map((route) => ({
-    url: `${siteConfig.url}${route}`,
+  const routePaths = Object.values(ROUTES);
+
+  const routes = routePaths.map((route) => ({
+    url: `${siteConfig.url.replace(/\/$/, "")}${route}`,
     lastModified: new Date().toISOString(),
   }));
 
-  return [...routes];
+  return routes;
 }
